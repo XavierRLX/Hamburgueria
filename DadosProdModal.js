4a8bba4d54f9cc17d4aff303c51bc688b65b8f30
Renan Xavier <renanlima2000.aer@gmail.com>
2023-03-06 15:54:59 -0300

    Adicionado LocalStorage e contador

--- DadosProdModal.js
+++ DadosProdModal.js
@@ -1,3 +1,8 @@
+// Atualiza o LocalStorage assim que a pag é carregada.
+window.addEventListener("load", function() {
+  exibirCarrinho();
+});
+
 // Passa os dados do card(produto) para o modal.
 // Seleciona todos os botões de compra
 const botoesCompra = document.querySelectorAll('[id^="comprarProduto"]');
@@ -5,10 +10,12 @@ const botoesCompra = document.querySelectorAll('[id^="comprarProduto"]');
 // Itera sobre os botões e adiciona um manipulador de eventos a cada um
 botoesCompra.forEach((botao, index) => {
   botao.addEventListener("click", function() {
+    // Obtém os valores dos campos do produto correspondente ao botão de compra clicado
     var nome = document.querySelectorAll(".nomeProduto")[index].textContent;
     var desc = document.querySelectorAll(".descProduto")[index].textContent;
     var preco = document.querySelectorAll(".preçoProduto")[index].value;
 
+    // Define os valores dos campos do modal com os valores do produto correspondente
     document.querySelector(".nomeProdutoModal").textContent = nome;
     document.querySelector(".descProdutoModal").textContent = desc;
     document.querySelector(".precoProdutoModal").value = preco;
@@ -17,7 +24,7 @@ botoesCompra.forEach((botao, index) => {
   });
 });
 
-// Pega o valor do produdo e faz multiplicação.
+// Pega o valor do produto e faz multiplicação.
 // seleciona os elementos HTML
 const btnMais = document.querySelector('.mais');
 const btnMenos = document.querySelector('.menos');
@@ -37,7 +44,7 @@ btnMais.addEventListener('click', function() {
 
 // adiciona evento de clique no botão "menos"
 btnMenos.addEventListener('click', function() {
-  // decrementa o valor da quantidade
+  // decrementa o valor da quantidade, mas não permite que seja menor que 1
   if (inputQuantidade.value > 1) {
     inputQuantidade.value--;
     
@@ -47,26 +54,29 @@ btnMenos.addEventListener('click', function() {
   }
 });
 
-// selecione o botão "Adicionar ao carrinho"
+// seleciona o botão "Adicionar ao carrinho"
 const btnAdicionar = document.querySelector(".btn-adc-carrinho");
 
-
+// Seleciona os botões de abrir e fechar carrinho, a lista de produtos e o campo de total do carrinho
 const btnAbrirCarrinho = document.querySelector("#btn-abrir-carrinho");
 const btnFecharCarrinho = document.querySelector("#btn-fechar-carrinho");
 const listaProdutos = document.querySelector("#lista-produtos");
 const totalCarrinho = document.querySelector("#total-carrinho");
 
-let carrinho = [];
+// Obtém os produtos do LocalStorage ou cria uma lista vazia
+let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
 
-let produtos = [];
-
+// adiciona evento de clique no botão "Adicionar ao carrinho"
 btnAdicionar.addEventListener("click", function() {
+  // Cria um objeto produto com o nome, preço e quantidade selecionados no modal
   const nomeProduto = document.querySelector(".nomeProdutoModal").textContent;
   const precoProduto = parseFloat(document.querySelector(".totalProdutoModal").value);
+  const quantidadeProduto = document.querySelector(".quantidade").value;
 
   const produto = {
     nome: nomeProduto,
-    preco: precoProduto
+    preco: precoProduto,
+    quantidade: quantidadeProduto
   };
 
   produtos.push(produto);
@@ -74,36 +84,52 @@ btnAdicionar.addEventListener("click", function() {
 });
 
 function exibirCarrinho() {
-  listaProdutos.innerHTML = "";
+  listaProdutos.innerHTML = "";  // Limpa a lista de produtos
   let total = 0;
 
-  produtos.forEach(function(produto, index) {
+  produtos.forEach(function (produto, index) { // Itera pelos produtos e cria elementos HTML para cada um deles
     const itemLista = document.createElement("li");
+
     const nomeProdutoSpan = document.createElement("span");
     nomeProdutoSpan.textContent = produto.nome;
-    const precoProdutoSpan = document.createElement("span");
-    precoProdutoSpan.textContent = "R$ " + produto.preco.toFixed(2);
-    const botaoExcluir = document.createElement("button");
-    botaoExcluir.textContent = "X";
-    botaoExcluir.addEventListener("click", function() {
-      produtos.splice(index, 1);
-      exibirCarrinho();
-    });
-    itemLista.appendChild(nomeProdutoSpan);
-    itemLista.appendChild(precoProdutoSpan);
-    itemLista.appendChild(botaoExcluir);
-    listaProdutos.appendChild(itemLista);
 
-    total += produto.preco;
+    const quantidadeProdutoSpan = document.createElement("span");
+    quantidadeProdutoSpan.textContent = " " + produto.quantidade + "x";
+
+    const precoProdutoSpan = document.createElement("span");
+    precoProdutoSpan.textContent = "  R$ " + produto.preco;
+
+    const botaoExcluir = document.createElement("button");  // Cria um botão para excluir o produto da lista
+    botaoExcluir.textContent = "";  // Define o texto do botão como vazio
+    botaoExcluir.classList.add("botao-excluir");  // Adiciona uma classe CSS ao botão
+    document.body.appendChild(botaoExcluir);    // Adiciona o botão ao DOM
+    botaoExcluir.addEventListener("click", function () {  // Adiciona um event listener ao botão que remove o produto da lista quando clicado
+      produtos.splice(index, 1);  // Remove o produto da lista de produtos
+      localStorage.setItem("produtos", JSON.stringify(produtos));  // Atualiza o localStorage com a nova lista de produtos
+      exibirCarrinho();  // Chama a função exibirCarrinho() novamente para atualizar a lista de produtos exibidos
+    });
+    itemLista.appendChild(nomeProdutoSpan);  // Adiciona o nome do produto ao item da lista
+    itemLista.appendChild(quantidadeProdutoSpan);  // Adiciona a quantidade do produto ao item da lista
+    itemLista.appendChild(precoProdutoSpan);  // Adiciona o preço do produto ao item da lista
+    itemLista.appendChild(botaoExcluir);  // Adiciona o botão de excluir ao item da lista
+    listaProdutos.appendChild(itemLista);  // Adiciona o item da lista à lista de produtos exibidos na tela
+
+    total += produto.preco;  // Soma o preço do produto ao total da compra
   });
 
-  totalCarrinho.textContent = total.toFixed(2);
+  totalCarrinho.textContent = total.toFixed(2);  // Exibe o total da compra na tela com duas casas decimais
+  
+  // Contador de produtos do icone.
+  const carrinhoCount = document.querySelector("#carrinho-count");  // Seleciona o elemento HTML que exibe o número de produtos no carrinho
+  carrinhoCount.textContent = produtos.reduce((total, produto) => total + parseInt(produto.quantidade), 0);  // Soma a quantidade de cada produto no carrinho e exibe o resultado no elemento selecionado acima
 
+  //Salva os produtos do localStorage
+  localStorage.setItem("produtos", JSON.stringify(produtos));  // Atualiza o localStorage com a nova lista de produtos
 
-
-  document.querySelector("#modal-carrinho").style.display = "block";
+  document.querySelector("#modal-carrinho").style.display = "block";  // Exibe o modal do carrinho na tela
 }
 
+ // Faz a verifição se o carrinho está vázio.
 const AbrirCarrinhoFlut = document.querySelector(".carrinho-compras");
 AbrirCarrinhoFlut.addEventListener("click", function () {
   const tituloCar = document.querySelector("#lista-produtos");
@@ -113,6 +139,7 @@ AbrirCarrinhoFlut.addEventListener("click", function () {
   } else {
     titloCarVazio.style.display = "none";
   }
+
 });
 
 
