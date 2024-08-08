3496bdf7c5ed0cf04da0c8075a94d3baa6ec6794
XavierRLX <renanlima2000.aer@gmail.com>
2024-08-08 15:47:17 -0300

    feat
    difusão, admPedidos/admProdutos. ,
    header/produtos.

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Servindo arquivos estáticos
+app.use('/style', express.static(path.join(__dirname, 'public/style')));
+app.use('/javaScript', express.static(path.join(__dirname, 'public/javaScript')));
+
+// Definindo rotas dinamicamente
+const routes = [
+  { path: '/', file: 'index.html' },
+  {path: '/principal', file: 'index.html'},
+  { path: '/pedido', file: 'pedido.html'},
+  { path: '/admPedidos', file: 'admPedidos.html'},
+  { path: '/admProdutos', file: 'admProdutos.html'}
+];
+
+routes.forEach(route => {
+  app.get(route.path, (req, res) => {
+    const indexPath = path.join(__dirname, `public/html/${route.file}`);
+    res.sendFile(indexPath);
+  });
+});
+
+// Inicializando o servidor
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
