ee71549c6ea9ed6251ce6e5b2d8b469c7fce8b4e
XavierRLX <renanlima2000.aer@gmail.com>
2025-01-28 10:30:27 -0300

    att
    atualizado o mapa "iframe" ao inves da api

--- public/javaScript/apiGoogle.js
+++ public/javaScript/apiGoogle.js
@@ -1,17 +1,13 @@
-function initMap() {
-    // Localização do endereço
-    var location = { lat: -22.8441, lng: -43.3011 }; // Coordenadas aproximadas para o endereço fornecido
 
-    // Criar o mapa
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: location
-    });
-
-    // Adicionar um marcador no mapa
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        title: 'Av. Brasil, 21779 - Guadalupe, Rio de Janeiro - RJ, 21670-000'
-    });
-}
+    //   function initMap() {
+    //     var location = { lat: -22.8441, lng: -43.3011 };
+    //     var map = new google.maps.Map(document.getElementById('map'), {
+    //       zoom: 15,
+    //       center: location
+    //     });
+    //     var marker = new google.maps.Marker({
+    //       position: location,
+    //       map: map,
+    //       title: 'Av. Brasil, 21779 - Guadalupe, Rio de Janeiro - RJ, 21670-000'
+    //     });
+    // }
