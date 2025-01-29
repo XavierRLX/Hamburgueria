6eaa928fe9bb3868ebcdcc954dcf7b4d027f1778
XavierRLX <renanlima2000.aer@gmail.com>
2025-01-29 10:01:43 -0300

    att
    Btn de baixar a tabela para excel.

--- public/javaScript/carregaPedidosAdm.js
+++ public/javaScript/carregaPedidosAdm.js
@@ -65,6 +65,37 @@ async function carregaPedidosPorStatus(status, containerId, loadingId, filterDat
     }
 }
 
+async function baixarPedidos() {
+    const url = `${supabaseUrl}/rest/v1/pedidos?select=*`;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': apiKey,
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        if (!response.ok) throw new Error("Erro ao obter dados do Supabase");
+
+        const data = await response.json();
+
+        // Criar a planilha com SheetJS
+        const ws = XLSX.utils.json_to_sheet(data);
+
+        // Criar o arquivo Excel
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "Pedidos");
+
+        // Baixar o arquivo
+        XLSX.writeFile(wb, "pedidos.xlsx");
+    } catch (error) {
+        alert('Erro ao baixar os pedidos.');
+        console.error(error);
+    }
+}
+
 async function carregaPedidosContagem() {
     const url = `${supabaseUrl}/rest/v1/pedidos?select=status`;
     const response = await fetch(url, {
