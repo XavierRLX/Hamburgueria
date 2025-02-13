f78c72ee89489c8010063fcd95628eacb192c142
XavierRLX <renanlima2000.aer@gmail.com>
2025-02-13 11:16:39 -0300

    removido caminhos ao banco

--- public/javaScript/creatUser.js
+++ public/javaScript/creatUser.js
@@ -4,10 +4,9 @@ const { createClient } = require('@supabase/supabase-js');
 const https = require('https');
 require('dotenv').config();
 
-const supabaseUrl = 'https://uweicybzciidmyumejzm.supabase.co';
-const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3ZWljeWJ6Y2lpZG15dW1lanptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3MjYzOTgsImV4cCI6MjA0NTMwMjM5OH0.xxcr3nzb0_bHISQvlBwiV0kDSNOieQa6eem7hbLc8Zk';
-const serviceRole = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3ZWljeWJ6Y2lpZG15dW1lanptIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTcyNjM5OCwiZXhwIjoyMDQ1MzAyMzk4fQ.LegQKtlJY5XyxAO09mc0mVmuKrx70PQneUrsz21at8E' ;
-const agent = new https.Agent({ rejectUnauthorized: false });
+const supabaseUrl = '1';
+const apiKey = '1';
+const serviceRole = '1' ;const agent = new https.Agent({ rejectUnauthorized: false });
 const supabase = createClient(supabaseUrl, serviceRole, { global: { fetch: (url, options) => fetch(url, { ...options, agent }) } });
 
 async function createUser(email, password, name, role = 'user') {
@@ -31,6 +30,3 @@ async function createUser(email, password, name, role = 'user') {
 
   console.log(`Usuário ${email} criado com sucesso como ${role}!`);
 }
-
-// // Criar um usuário admin para teste
-// createUser('dreamslanchesrj@gmail.com', 'lanchoneteDL24@', 'Administrador', 'admin');
